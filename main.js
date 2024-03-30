03d5f64c17a19fd34159b3c8435096ff415c6178
deymion <86213360+deymion@users.noreply.github.com>
2024-03-30 15:08:29 +0800

    Implement 'createGrid()' for square div grid generation

---
+++ main.js
@@ -0,0 +1,27 @@
+container = document.querySelector(".container");
+
+createGrid(16, 16);
+
+function createGrid(rows = 1, cols = 1) {
+
+    for (let i = 0; i < rows; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+
+        for (let j = 0; j < cols; j++) {
+            const col = document.createElement("div");
+            col.setAttribute("class", "col");
+            row.appendChild(col);
+        }
+        container.appendChild(row);
+    }
+}
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+}
+
+function randomColor() {
+    return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
+}
